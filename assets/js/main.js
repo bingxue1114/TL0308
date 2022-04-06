4861f8ed2a683861892999d4739681360d32d105
LAPTOP-5HC9EN9R\bingxue <91114bingxue>
2022-04-07 01:38:31 +0800

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,9 @@
 
         let weeks = Math.floor(timeleft / (1000 * 60 * 60 * 24 * 7));
         let days = Math.floor(timeleft / (1000 * 60 * 60 * -1));
-        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) * -1);
+        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60) * -1);
+        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000 * -1);
 
         let output = countdown.getAttribute('data-template');
         output = output.replace('%w', weeks).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
