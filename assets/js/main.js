5a342b3cecd0d1553793b5e8bf3e87c850b9c6d5
LAPTOP-5HC9EN9R\bingxue <91114bingxue>
2022-04-07 12:33:02 +0800

    online v1.0 time ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,13 +26,13 @@
     let countdown = select('.countdown');
 
     const countDownDate = function() {
-        let timeleft = new Date().getTime() - new Date(countdown.getAttribute('data-count').getTime());
+        let timeleft = new Date().getTime() - new Date(countdown.getAttribute('data-count')).getTime();
 
         let weeks = Math.floor(timeleft / (1000 * 60 * 60 * 24 * 7));
-        let days = Math.floor(timeleft / (1000 * 60 * 60 * -1));
-        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) * -1);
-        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60) * -1);
-        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000 * -1);
+        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
         let output = countdown.getAttribute('data-template');
         output = output.replace('%w', weeks).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
